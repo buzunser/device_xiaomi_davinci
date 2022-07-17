03d7604e184e4afeaa38f3c076a380cd7aaedb1a
buzunser <79738404+buzunser@users.noreply.github.com>
2022-07-17 19:39:39 +0300

    davinci: Move to common fingerprint HIDL

--- device.mk
+++ device.mk
@@ -41,7 +41,6 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sm6150-udfps \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
     
 # Fastcharge    
